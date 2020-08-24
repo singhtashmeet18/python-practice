fb61989f95e39fbf0ab6dfd6a4263ed17501ab54
Panny <panny@gmail.com>
2020-08-24 18:54:11 +0530

    basics of puython

---
+++ basics.py
@@ -0,0 +1,47 @@
+#print ("hello world")
+#paragraph = """This is a paragraph.it is made up of multiple lines and segments."""
+#print (paragraph)
+
+#variabes
+counter = 100       #An integer assignment
+miles = 100.0       #A floating point
+name = ("virat")    #A string
+
+#print (counter)
+#print (miles)
+#print (name)
+
+#PYTHON STRINGS
+
+#str = "HELOO WORLD"
+#print (str)
+#print (str[2:5])
+#print (str[2:])
+#print (str * 2)
+#print (str + "TEST")
+
+
+#PYTHON LIST
+
+#list = ['abcd',786,20.2,"virat kohli",70.2]
+#tinylist = [123,"tashmeet singh"]
+#print (list)
+#print (list[0])
+#print (list[0:3])
+#print (list[1:4])
+#print (list[2:])
+#print (list[2:4])
+#print (tinylist * 2)
+#print (list + tinylist)
+
+
+#Python tuple
+tuple = ('abcd',65,'rohit sharma',98.9,)
+tinytuple = (162.2,'mohnish')
+
+#print (tuple)
+#print (tuple[2:4])
+#print (tuple[1:4])
+#print (tinytuple * 2)
+#print (tuple + tinytuple)
+
