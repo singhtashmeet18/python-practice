d0d9b61159faffba0910c81bd0732463fb63a233
Panny <panny@gmail.com>
2020-08-27 17:20:10 +0530

    accesing value to strings

---
+++ basics1.py
@@ -0,0 +1,10 @@
+#SINGLE STATEMENT SUITES
+#var = 10
+#if (var == 10) : print ("value of variable is 10") : print ("goodbye")
+
+#ACCESING VALUE IN STRINGS
+var1 = "heloo world"
+var2 = "python programming"
+print ("var1[0] %s "  % var1[0])
+print ("var2[1:5] %s " % var2[1:5])
+print ("updated string:" ,var1[:6] + 'python')
