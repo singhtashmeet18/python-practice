7a3bd54c3fc64e6f9e2659e9a4fc22217338c73e
Panny <panny@gmail.com>
2020-08-28 15:30:29 +0530

    if else statements

---
+++ ifelse.py
@@ -0,0 +1,9 @@
+num = float(input("enter a number:"))
+a = num
+if (a>0):
+    print ("It is positive number")
+elif (a == 0):
+    print ("It is zero")
+else:
+    print ("It is negative number")
+
