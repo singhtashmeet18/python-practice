aef7a632c4df22214df859b2cb13201ef9deb091
Panny <panny@gmail.com>
2020-08-29 17:14:35 +0530

    for loop

---
+++ forloop.py
@@ -0,0 +1,14 @@
+#PROGRAM TO FIND THE SUM OF ALL NUMBERS STORED IN LIST
+numbers = [3,4,5,6,7,8,10]#list of numbers
+sum = 0#variable to store the sum
+for val in numbers:
+    sum = sum + val
+#print("sum of all numbers is %s" %(sum))
+
+#RANGE() FUNCTION
+
+#print(list(range10)))
+#print(list(range(2,8)))
+print(list(range(2,20,3)))
+
+
